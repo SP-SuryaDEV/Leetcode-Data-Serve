9f079acc319e751c2b8bdfc50391c8d565bb2246
RUDHRESH S <118143270+iamrudhresh@users.noreply.github.com>
2024-03-22 11:54:12 +0530

    Add files via upload

---
+++ Checker.py
@@ -0,0 +1,57 @@
+import streamlit as st
+import json
+import pandas as pd
+from Fetcher import contestfetch
+from streamlit_gsheets import GSheetsConnection
+
+def fetch_input_data():
+    conn = st.experimental_connection('gsheets', type=GSheetsConnection)
+    spreadsheet_url = 'https://docs.google.com/spreadsheets/d/1d0foz9yjxydkXHDHtdNFf0LQtnIuneowUnRYqUx6vzY/'
+    
+    df = conn.read(worksheet="Input", usecols=list(range(9)))
+    df.drop(how='all')
+
+    st.write(df)
+
+    return df
+
+st.title("Contest Data Fetch")
+
+data = None
+
+def LeetcodeContestInstantFetch(contest_name):
+    try:
+        data = contestfetch(contest_name)
+        
+    except json.JSONDecodeError:
+        st.error("Invalid JSON file. Please upload a valid JSON file.")
+    csv = None
+
+    if data is not None:
+        csv = fetch_input_data()
+
+        csv['Rank'] = ''
+        csv['ProbCount'] = ''
+        csv['Score'] = ''
+
+        for ind , row in csv.iterrows():
+            username = row['Username'].lower()
+
+            if username in data:
+                csv.loc[ind,'Rank'] = data[username]['rank']
+                csv.loc[ind,'ProbCount'] = data[username]['problemsSolved']
+                csv.loc[ind,'Score'] = data[username]['score']
+            else:
+                csv.loc[ind,'Rank'] = 0
+                csv.loc[ind,'ProbCount'] = 0
+                csv.loc[ind,'Score'] = 0
+
+    conn = st.experimental_connection('gsheets', type=GSheetsConnection)
+    
+    conn.create(worksheet=f"{contest_name.title()}", data=csv)
+
+    st.sucess('Data Fetched...')
+
+            
+
+LeetcodeContestInstantFetch('weekly-contest-389')
---
+++ Fetcher.py
@@ -0,0 +1,89 @@
+import requests
+import backoff
+import pandas as pd
+import streamlit as st
+from streamlit_gsheets import GSheetsConnection
+
+def read_usernames():
+    spreadsheet_url = 'https://docs.google.com/spreadsheets/d/1d0foz9yjxydkXHDHtdNFf0LQtnIuneowUnRYqUx6vzY/'
+    conn = st.experimental_connection("gsheets", type=GSheetsConnection)
+    df = conn.read(worksheet="Input", usecols=list(range(9)))
+    df.drop(how='all')
+    return list(df['Username'].str.lower())
+
+class ForbiddenError(Exception):
+    pass
+
+@backoff.on_exception(backoff.expo, ForbiddenError, max_tries=20)
+def fetch(contestName,pageNumber):
+
+    headers = {
+    'Content-Type': 'application/json',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
+    'cookies' : 'asdweaea',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9'
+    }
+    
+    url = f"https://leetcode.com/contest/api/ranking/{contestName}/?pagination={pageNumber}&region=global"
+    response = requests.get(url , headers=headers)
+
+    if response.status_code == 200:
+        return response
+    elif response.status_code == 403:
+        print('error raised')
+        raise ForbiddenError('forbidden')
+    else:
+        return response
+
+
+def contestfetch(contestName):
+    pageNumber = 1
+    completeData = {}
+    
+    usernames = read_usernames()
+
+    while(True):
+        
+        response = fetch(contestName,pageNumber)
+
+        if response.status_code == 200:
+            json_data = response.json()
+
+            completeData['found'] = True
+
+            if len(json_data) == 0:
+                
+                break
+            
+            submissions = json_data['submissions']
+            total = json_data['total_rank']
+
+
+
+            if(len(submissions) == 0):
+                break
+
+            for i in range(len(submissions)):
+                
+                userName = total[i]['username'].lower()
+
+                if userName in usernames:
+                    rank = total[i]['rank']+1
+                    score = total[i]['score']
+                    problemsSolved = len(submissions[i])
+
+                    completeData[userName] = {'rank':rank,"score":score, "problemsSolved" : problemsSolved}
+            
+            print(pageNumber)
+            pageNumber += 1
+        elif response.status_code == 404:
+            return {'found' : 400} , False
+        else:
+
+            return {'found' : response.status_code} , False
+        
+
+    return completeData , True
+
+
+    
